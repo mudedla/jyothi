fa45f09cb647b8b28415a2faaf75a46ee7f7dcb2
User <User@192.168.0.133>
2019-02-22 18:09:04 +0530

    java1

---
+++ src/chaitu/Sample1.java
@@ -0,0 +1,23 @@
+package chaitu;
+
+public class Sample1
+{
+	//properties
+	public int x;
+	public float y;
+	public char z;
+    public boolean w;
+    //methods
+    public void display()
+    {
+    	System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(w);
+    }
+    
+    
+    
+    
+    
+}
