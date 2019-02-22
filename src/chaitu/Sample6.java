497d971b3ea6a034fb81e42d9d32ecc8da24bf84
User <User@192.168.0.133>
2019-02-22 18:19:03 +0530

    java1

---
+++ src/chaitu/Sample6.java
@@ -0,0 +1,27 @@
+package chaitu;
+
+public interface Sample6
+{
+	/*public int addition (int x,int y);
+	public int subtarct (int x,int y);
+	public int multiply (int x,int y);
+	public int Divided (int x,int y);
+}
+     public class Sample7 implements Sample6
+{
+	public int addition (int x, int y)
+	{
+		*/
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+
+
