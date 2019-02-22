ef2be74df1e9d2d248c86bd95193b549bd613726
User <User@192.168.0.133>
2019-02-22 18:13:40 +0530

    java1

---
+++ src/chaitu/Sample3.java
@@ -0,0 +1,30 @@
+package chaitu;
+
+public class Sample3
+{
+	//properties
+			public int x;
+			public float y;
+			public char z;
+		    public boolean w;
+		    public String s;
+		    //constructor method
+		    public Sample3 (int a, char b, String c)
+		    {
+		    	x=a;
+		    	z=b;
+		    	s=c;
+		    	
+		    }
+
+//operational method
+
+public void display()
+{
+	System.out.println(x);
+    System.out.println(y);
+    System.out.println(z);
+    System.out.println(w);
+    System.out.println(s);
+}
+}
